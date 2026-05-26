f7329943f57f172f5b45f5e86474754aa3903e68
mohak <mohakchoubey@gmail.com>
2026-05-26 10:02:37 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,19 @@
-<script> const fadeElements = document.querySelectorAll('.fade'); const observer = new IntersectionObserver((entries)=>{ entries.forEach((entry)=>{ if(entry.isIntersecting){ entry.target.classList.add('show'); } }); }, { threshold:0.2 }); fadeElements.forEach((el)=>{ observer.observe(el); }); </script>
+
+
+    const fadeElements = document.querySelectorAll('.fade');
+
+    const observer = new IntersectionObserver((entries)=>{
+      entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+          entry.target.classList.add('show');
+        }
+      });
+    }, {
+      threshold:0.2
+    });
+
+    fadeElements.forEach((el)=>{
+      observer.observe(el);
+    });
+
+  
